5a413561764c4daa0195a4e137793e62c6ab4396
乔治 <18890176107@163.com>
2022-10-31 09:48:09 +0800

    ref 属性

---
+++ vue_test/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1 v-text="masg"></h1>
+        <button @click='showDOM'>点我输出上方 DOM 元素</button>
+        <school/>
+    </div>
+</template>
+
+<script>
+    //引入 School 组件
+    import School from './components/School.vue'
+    export default {
+        name: 'App',
+        components: { School },
+        data() {
+            return {
+                masg: '你好'
+            }
+        },
+        methods: {
+            showDOM(){
+                console.log('hhhh')
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
---
+++ vue_test/src/main.js
@@ -0,0 +1,12 @@
+//引入 Vue
+import Vue from 'vue'
+//引入 App
+import App from './App'
+//关闭 Vue 产生的提示
+Vue.config.productionTip = false
+
+//创建 vm
+new Vue({
+    el: "#app",
+    render: h => h(App)
+})
