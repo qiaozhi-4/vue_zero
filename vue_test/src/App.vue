4f703faac5ab8b0596b1c4e90fc7b2dc9f2992af
乔治 <18890176107@163.com>
2022-10-31 18:53:25 +0800

    TodoList 案例

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,30 +1,101 @@
 <template>
-    <div>
-        <h1 v-text="masg"></h1>
-        <button @click='showDOM'>点我输出上方 DOM 元素</button>
-        <school/>
+    <div id="root">
+        <div class="todo-container">
+            <div class="todo-wrap">
+                <MyHeader :addTodo="addTodo"/>
+                <MyList :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo"/>
+                <MyFooter :todos="todos" :checkAllTodo="checkAllTodo" :clearAllTodo="clearAllTodo"/>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     //引入 School 组件
-    import School from './components/School.vue'
+    import MyFooter from './components/MyFooter.vue'
+    import MyHeader from './components/MyHeader.vue'
+    import MyList from './components/MyList.vue'
+
     export default {
         name: 'App',
-        components: { School },
+        components: { MyFooter,MyHeader,MyList },
         data() {
             return {
-                masg: '你好'
+                todos:[
+                {id:'001',title:'吃饭',done:true},
+                {id:'002',title:'睡觉',done:false},
+                {id:'003',title:'打豆豆',done:true},
+                ]
             }
         },
         methods: {
-            showDOM(){
-                console.log('')
+            addTodo(todo){
+                this.todos.unshift(todo)
+            },
+			//勾选or取消勾选一个todo
+			checkTodo(id){
+				this.todos.forEach((todo)=>{
+					if(todo.id === id) todo.done = !todo.done
+				})
+			},
+			//勾选or取消勾选一个todo
+			deleteTodo(id){
+				this.todos = this.todos.filter((todo) => {
+                    return todo.id !== id
+                })
+			},
+            //全选或者全不选
+            checkAllTodo(done){
+                 this.todos.forEach((todo) =>{
+                    todo.done = done
+                 })
+            },
+            //全选或者全不选
+            clearAllTodo(){
+                 this.todos = this.todos.filter((todo) => {
+                    return !todo.done
+                })
             }
         },
     }
 </script>
 
 <style>
-
+    /*base*/
+    body {
+    background: #fff;
+    }
+    .btn {
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+    }
+    .btn-danger {
+    color: #fff;
+    background-color: #da4f49;
+    border: 1px solid #bd362f;
+    }
+    .btn-danger:hover {
+    color: #fff;
+    background-color: #bd362f;
+    }
+    .btn:focus {
+    outline: none;
+    }
+    .todo-container {
+    width: 600px;
+    margin: 0 auto;
+    }
+    .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    }
 </style>
---
+++ vue_test/src/components/MyItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <li>
+    <label>
+        <input type="checkbox" :checked="todoInfo.done" @change="handleCheck(todoInfo.id)"/>
+        <!-- <input type="checkbox" v-model="todoInfo.done"/> -->
+        <span>{{todoInfo.title}}</span>
+    </label>
+    <button class="btn btn-danger" @click="handleDelete(todoInfo.id)">删除</button>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'MyItem',
+    props:['todoInfo','checkTodo','deleteTodo'],
+    methods: {
+			//勾选or取消勾选
+			handleCheck(id){
+				//通知App组件将对应的todo对象的done值取反
+				this.checkTodo(id)
+			},
+			//删除一行
+			handleDelete(id){
+				//通知App组件将对应的todo对象删除
+				//this.deleteTodo(id)
+        if(confirm('确定删除吗？')) this.deleteTodo(id)
+      },
+    }
+  }
+</script>
+
+<style scoped>
+  /*item*/
+  li {
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #ddd;
+  }
+
+  li label {
+    float: left;
+    cursor: pointer;
+  }
+
+  li label li input {
+    vertical-align: middle;
+    margin-right: 6px;
+    position: relative;
+    top: -1px;
+  }
+
+  li button {
+    float: right;
+    display: none;
+    margin-top: 3px;
+  }
+
+  li:before {
+    content: initial;
+  }
+
+  li:last-child {
+    border-bottom: none;
+  }
+  li:hover{
+    background-color: rgb(155, 150, 150);
+  }
+  li:hover button{
+    display: block;
+  }
+</style>
