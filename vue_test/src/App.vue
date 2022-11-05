dfc3d59314488f8940ac759310e93367b90833cf
乔治 <18890176107@163.com>
2022-11-05 21:33:29 +0800

    github搜索案例

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,63 +1,7 @@
 <template>
-  <div>
-    <div class="container">
-      <section class="jumbotron">
-        <h3 class="jumbotron-heading">Search Github Users</h3>
-        <div>
-          <input
-            type="text"
-            placeholder="enter the name you search"
-          />&nbsp;<button>Search</button>
-        </div>
-      </section>
-      <div class="row">
-        <div class="card">
-          <a href="https://github.com/xxxxxx" target="_blank">
-            <img
-              src="https://cn.vuejs.org/images/logo.svg"
-              style="width: 100px"
-            />
-          </a>
-          <p class="card-text">xxxxxx</p>
-        </div>
-        <div class="card">
-          <a href="https://github.com/xxxxxx" target="_blank">
-            <img
-              src="https://cn.vuejs.org/images/logo.svg"
-              style="width: 100px"
-            />
-          </a>
-          <p class="card-text">xxxxxx</p>
-        </div>
-        <div class="card">
-          <a href="https://github.com/xxxxxx" target="_blank">
-            <img
-              src="https://cn.vuejs.org/images/logo.svg"
-              style="width: 100px"
-            />
-          </a>
-          <p class="card-text">xxxxxx</p>
-        </div>
-        <div class="card">
-          <a href="https://github.com/xxxxxx" target="_blank">
-            <img
-              src="https://cn.vuejs.org/images/logo.svg"
-              style="width: 100px"
-            />
-          </a>
-          <p class="card-text">xxxxxx</p>
-        </div>
-        <div class="card">
-          <a href="https://github.com/xxxxxx" target="_blank">
-            <img
-              src="https://cn.vuejs.org/images/logo.svg"
-              style="width: 100px"
-            />
-          </a>
-          <p class="card-text">xxxxxx</p>
-        </div>
-      </div>
-    </div>
+  <div class="container">
+    <MySearch/>
+    <MyList/>
   </div>
 </template>
 
@@ -71,29 +15,4 @@
 	};
 </script>
 
-<style>
-	.album {
-		min-height: 50rem; /* Can be removed; just added for demo purposes */
-		padding-top: 3rem;
-		padding-bottom: 3rem;
-		background-color: #f7f7f7;
-	}
 
-	.card {
-		float: left;
-		width: 33.333%;
-		padding: 0.75rem;
-		margin-bottom: 2rem;
-		border: 1px solid #efefef;
-		text-align: center;
-	}
-
-	.card > img {
-		margin-bottom: 0.75rem;
-		border-radius: 100px;
-	}
-
-	.card-text {
-		font-size: 85%;
-	}
-</style>
--- vue_test/src/components/MySearch.vue
+++ vue_test/src/components/MySearch.vue
@@ -1,5 +1,13 @@
 <template>
-  
+	<section class="jumbotron">
+		<h3 class="jumbotron-heading">Search Github Users</h3>
+		<div>
+			<input
+			type="text"
+			placeholder="enter the name you search"
+			/>&nbsp;<button>Search</button>
+		</div>
+	</section>
 </template>
 
 <script>
