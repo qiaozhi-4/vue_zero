e9aa37df6cebc37a39b91f019dde8db73765cb57
乔治 <18890176107@163.com>
2022-11-15 19:37:54 +0800

    git hup 动态组件

--- vue_test/src/components/MyList.vue
+++ vue_test/src/components/MyList.vue
@@ -1,83 +1,54 @@
 <template>
-	<div class="row">
-		<div class="card">
-			<a href="https://github.com/xxxxxx" target="_blank">
-			<img
-				src="https://cn.vuejs.org/images/logo.svg"
-				style="width: 100px"
-			/>
-			</a>
-			<p class="card-text">xxxxxx</p>
-		</div>
-		<div class="card">
-			<a href="https://github.com/xxxxxx" target="_blank">
-			<img
-				src="https://cn.vuejs.org/images/logo.svg"
-				style="width: 100px"
-			/>
-			</a>
-			<p class="card-text">xxxxxx</p>
-		</div>
-		<div class="card">
-			<a href="https://github.com/xxxxxx" target="_blank">
-			<img
-				src="https://cn.vuejs.org/images/logo.svg"
-				style="width: 100px"
-			/>
-			</a>
-			<p class="card-text">xxxxxx</p>
-		</div>
-		<div class="card">
-			<a href="https://github.com/xxxxxx" target="_blank">
-			<img
-				src="https://cn.vuejs.org/images/logo.svg"
-				style="width: 100px"
-			/>
-			</a>
-			<p class="card-text">xxxxxx</p>
-		</div>
-		<div class="card">
-			<a href="https://github.com/xxxxxx" target="_blank">
-			<img
-				src="https://cn.vuejs.org/images/logo.svg"
-				style="width: 100px"
-			/>
-			</a>
-			<p class="card-text">xxxxxx</p>
-		</div>
-	</div>
+  <div class="row">
+    <div class="card" v-for="user in users" :key="user.login">
+      <a :href="user.html_url" target="_blank">
+        <img :src="user.avatar_url" style="width: 100px" />
+      </a>
+      <p class="card-text">{{user.login}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name:'MyList'
-
-}
+  name: "MyList",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.$bus.$on("getUsers", (users) => {
+      console.log("我是List组件,收到数据:", users);
+	  this.users = users
+    });
+  },
+};
 </script>
 
 <style scoped>
-	.album {
-		min-height: 50rem; /* Can be removed; just added for demo purposes */
-		padding-top: 3rem;
-		padding-bottom: 3rem;
-		background-color: #f7f7f7;
-	}
+.album {
+  min-height: 50rem; /* Can be removed; just added for demo purposes */
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  background-color: #f7f7f7;
+}
 
-	.card {
-		float: left;
-		width: 33.333%;
-		padding: 0.75rem;
-		margin-bottom: 2rem;
-		border: 1px solid #efefef;
-		text-align: center;
-	}
+.card {
+  float: left;
+  width: 33.333%;
+  padding: 0.75rem;
+  margin-bottom: 2rem;
+  border: 1px solid #efefef;
+  text-align: center;
+}
 
-	.card > img {
-		margin-bottom: 0.75rem;
-		border-radius: 100px;
-	}
+.card > img {
+  margin-bottom: 0.75rem;
+  border-radius: 100px;
+}
 
-	.card-text {
-		font-size: 85%;
-	}
+.card-text {
+  font-size: 85%;
+}
 </style>
--- vue_test/src/components/MySearch.vue
+++ vue_test/src/components/MySearch.vue
@@ -1,21 +1,42 @@
 <template>
-	<section class="jumbotron">
-		<h3 class="jumbotron-heading">Search Github Users</h3>
-		<div>
-			<input
-			type="text"
-			placeholder="enter the name you search"
-			/>&nbsp;<button>Search</button>
-		</div>
-	</section>
+  <section class="jumbotron">
+    <h3 class="jumbotron-heading">Search Github Users</h3>
+    <div>
+      <input
+        type="text"
+        placeholder="enter the name you search"
+        v-model="keyWord"
+      />&nbsp;
+      <button @click="searchUsers">Search</button>
+    </div>
+  </section>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-	name:'MySearch'
-}
+  name: "MySearch",
+  data() {
+	return {
+		keyWord:''
+	}
+  },
+  methods: {
+    searchUsers() {
+      console.log(this.keyWord);
+      axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
+        (response) => {
+          console.log("请求成功:", response.data.items);
+		  this.$bus.$emit('getUsers', response.data.items)
+        },
+        (error) => {
+          console.log("请求失败:", error.message);
+        }
+      );
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
