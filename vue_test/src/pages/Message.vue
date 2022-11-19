8afcf14f77d90b085c7665a9589f63eb99835391
乔治 <18890176107@163.com>
2022-11-19 10:35:19 +0800

    路由的query参数

--- vue_test/src/pages/Message.vue
+++ vue_test/src/pages/Message.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
     <ul>
-      <li><a href="/mx1">message001</a>&nbsp;&nbsp;</li>
-      <li><a href="/message2">message002</a>&nbsp;&nbsp;</li>
-      <li><a href="/message/3">message003</a>&nbsp;&nbsp;</li>
+      <li v-for="msg in messageList" :key="msg.id">
+        <router-link
+          active-class="active"
+          :to="{
+            path: '/home/message/detail',
+            query:{
+                id:msg.id,
+                title:msg.title
+            }
+          }">
+          {{ msg.title }}
+        </router-link>
+      </li>
     </ul>
+    <hr />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
   name: "Message",
+  data() {
+    return {
+      messageList: [
+        { id: "001", title: "消息001" },
+        { id: "002", title: "消息002" },
+        { id: "003", title: "消息003" },
+      ],
+    };
+  },
 };
 </script>
--- vue_test/src/router/index.js
+++ vue_test/src/router/index.js
@@ -3,6 +3,7 @@ import About from '../pages/About'
 import Home from '../pages/Home'
 import Message from '../pages/Message'
 import News from '../pages/News'
+import Detail from '../pages/Detail'
 
 export default new VueRouter({
     routes: [
@@ -16,7 +17,13 @@ export default new VueRouter({
             children: [
                 {
                     path: 'message',
-                    component: Message
+                    component: Message,
+                    children:[
+                        {
+                            path:'detail',
+                            component: Detail
+                        }
+                    ]
                 },
                 {
                     path: 'news',
